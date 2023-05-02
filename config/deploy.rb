29034a2a123cd2166a91bade05b4006494e1e04d
felix1251 <domhack123@gmail.com>
2023-05-03 06:47:40 +0800

    at fisrt deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
-# set :initial, true
-# # run only if app is being deployed for the very first time, should update "set :initial, true" above to run this
-# before 'deploy:migrate', 'database:create' if fetch(:initial)
+set :initial, true
+# run only if app is being deployed for the very first time, should update "set :initial, true" above to run this
+before 'deploy:migrate', 'database:create' if fetch(:initial)
+after 'deploy:migrate', 'database:seed' if fetch(:initial)
