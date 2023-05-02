f5093bcf0b0b234c6639226fe929de1b06f6e865
felix1251 <domhack123@gmail.com>
2023-05-03 06:22:44 +0800

    ignore env

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,11 @@
 #Authors
 author1 = Author.create(first_name: "Rainer Steel", last_name: "Rilke")
 author2 = Author.create(first_name: "Joel", last_name: "Hartse")
-author3 = Author.create(first_name: "Hannah", middle_name: "P.", last_name: "Hannah")
+author3 = Author.create(first_name: "Hannah", middle_name: "P.", last_name: "Templer")
 author4 = Author.create(first_name: "Marguerite", middle_name: "Z.", last_name: "Duras")
 author5 = Author.create(first_name: "Kingsley", last_name: "Amis")
+author6 = Author.create(first_name: "Iceberg Nash", last_name: "Slim")
+author7 = Author.create(first_name: "Camille Byron", last_name: "Paglia")
 
 #Publischers
 pub1 = Publisher.create(name: "McSweeney's")
@@ -31,6 +33,9 @@ book3 = Book.create(title: "Cosmoknights", isbn_10: "1-603-09454-7",
 book4 = Book.create(title: "Essex County", isbn_10: "1-603-09038-X", 
     publication_year: "1990", publisher_id: pub3.id, price: "500"
 )
+book5 = Book.create(title: "Doughnuts And Doom", isbn_13: "978-1-603095-13-6", 
+    publication_year: "2022", publisher_id: pub1.id, price: "100"
+)
 
 #BookAuthors
 BookAuthor.create(book_id: book1.id, author_id: author1.id)
@@ -40,8 +45,14 @@ BookAuthor.create(book_id: book2.id, author_id: author4.id)
 BookAuthor.create(book_id: book3.id, author_id: author5.id)
 BookAuthor.create(book_id: book4.id, author_id: author5.id)
 
+BookAuthor.create(book_id: book5.id, author_id: author3.id)
+BookAuthor.create(book_id: book5.id, author_id: author4.id)
+BookAuthor.create(book_id: book5.id, author_id: author6.id)
+BookAuthor.create(book_id: book5.id, author_id: author7.id)
+
 #Books image cover
 book1.cover.attach(io: File.open("app/assets/images/The_Underwater_Welder.png"), filename: "The_Bend_of_Luck.png")
 book2.cover.attach(io: File.open("app/assets/images/American_Elf.jpg"), filename: "American_Elf.jpg")
 book3.cover.attach(io: File.open("app/assets/images/Cosmoknights.jpg"), filename: "Cosmoknights.jpg")
 book4.cover.attach(io: File.open("app/assets/images/Essex_County.jpeg"), filename: "Essex_County.jpg")
+book5.cover.attach(io: File.open("app/assets/images/doughnuts_&_Doom.png"), filename: "doughnuts_&_Doom.png")
