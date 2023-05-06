08f44f3885519457ef046c3d2659c16da69430cb
felix1251 <domhack123@gmail.com>
2023-05-06 11:08:10 +0800

    initial deploy capistano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,29 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
-# set :initial, true
+namespace :custom_task do
+    task :create_db do
+        on roles(:db) do
+            exec("rake db:create")
+        end  
+    end
+
+    task :seed_db do
+        on roles(:db) do
+            exec("rake db:seed")
+        end
+    end
+end
+
 # # run only if app is being deployed for the very first time, should update "set :initial, true" above to run this
-# before 'deploy:migrate', 'database:create' if fetch(:initial)
+
+set :initial, false
+
+if fetch(:initial)
+    before 'deploy:migrate', 'custom_task:create_db'
+    after 'deploy:migrate', 'custom_task:seed_db'
+end
+
+
+
+
